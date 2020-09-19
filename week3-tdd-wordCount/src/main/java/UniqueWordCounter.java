39a440f75cee364f8ab7febdb7baddfd309617ed
Meltem Suiçmez <41024883+Melteos@users.noreply.github.com>
2020-09-19 11:05:45 +0300

    added capital letter case

--- week3-tdd-wordCount/src/main/java/UniqueWordCounter.java
+++ week3-tdd-wordCount/src/main/java/UniqueWordCounter.java
@@ -7,7 +7,10 @@ public class UniqueWordCounter {
     public int countUniqueWords(String sentence) {
         if(sentence.equals(""))
             return 0;
-        String[] words = sentence.split("[\\W&&[^']]+");
+        String[] words = sentence.split("[^\\w+'?\\w*]");
+        for (int i=0; i<words.length; i++) {
+            words[i] = words[i].toLowerCase();
+        }
         Set<String> uniqueWords = new HashSet<String>(Arrays.asList(words));
         return uniqueWords.size();
     }
